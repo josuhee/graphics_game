72cf7fec3ee7768ef90a7fd7f317b20403bc1ba6
josuhee <suheeqwe@gmail.com>
2021-09-27 11:51:49 +0900

    screen

---
+++ ConsoleGame/ConsoleGame/ConsoleGame.cpp
@@ -0,0 +1,24 @@
+﻿#include <GL/glut.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+
+void MyDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, 300, 300);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Test");
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
+    return 0;
+}
